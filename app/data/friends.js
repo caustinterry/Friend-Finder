bb276056ead91dbb312f47bbd23b128db68e5154
Mr. T <austinbook@Christophers-MacBook-Pro.local>
2019-09-11 13:18:49 -0600

    Created files and started initial JS

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friendsArray = [
+  {
+    name: "Cage Hunt", //Tom Cruise
+    photo: "",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Neely O'Hara", //Jessica Alba
+    photo: "",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Carl Con Carne", //Brad Pitt
+    photo: "",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Humphrey Handbag", //Elton John
+    photo: "",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Ingrid Jackson", //Queen B
+    photo: "",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
+
+module.exports = friendsArray;
